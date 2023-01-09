8a1e69e7cfec9899f91d370cea23cd6d7d563a86
이지민 <68285285+jmlee119@users.noreply.github.com>
2023-01-09 12:39:38 +0900

    Add files via upload

---
+++ dynamic_beat_1/KeyListener.java
@@ -0,0 +1,64 @@
+package dynamic_beat_1;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyListener extends KeyAdapter {
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (DynamicBeat.game ==null ) {
+			return;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+			DynamicBeat.game.PressS();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_D) {
+			DynamicBeat.game.PressD();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_F) {
+			DynamicBeat.game.PressF();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			DynamicBeat.game.PressSpace();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_J) {
+			DynamicBeat.game.PressJ();
+		} 
+		else if (e.getKeyCode() == KeyEvent.VK_K) {
+			DynamicBeat.game.PressK();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_L) {
+			DynamicBeat.game.PressL();
+		}
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e ) {
+		if (DynamicBeat.game ==null ) {
+			return;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+			DynamicBeat.game.releaseS();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_D) {
+			DynamicBeat.game.releaseD();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_F) {
+			DynamicBeat.game.releaseF();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			DynamicBeat.game.releaseSpace();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_J) {
+			DynamicBeat.game.releaseJ();
+		} 
+		else if (e.getKeyCode() == KeyEvent.VK_K) {
+			DynamicBeat.game.releaseK();
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_L) {
+			DynamicBeat.game.releaseL();
+		}
+	}
+}
